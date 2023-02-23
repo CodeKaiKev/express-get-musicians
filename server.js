f9a98c39e65311fc48b558ef23749eff1dc61622
Codekaikev <codekaikev@gmail.com>
2023-02-23 00:45:05 +0000

    Pt 3

--- server.js
+++ server.js
@@ -11,15 +11,36 @@ app.get("/musicians", async (req, res) => {
 //TODO
 app.get("/musicians/:id", async (req, res) => {
     const musician = await Musician.findByPk(req.params.id);
-    res.json(musician);
+    res.json({musician});
 })
 
-// app.listen(port, () => {
-//     sequelize.sync();
-//     console.log("Listening on port http://localhost:" + port + "/musicians")
-// })
+app.use(express.json());
+//Post for new musicians
+app.post("/musicians", async ( request, response) => {
+    const newMusicians = await Musician.create(request.body);
+    response.json(Musician);
+})
+//Put for updating a new musician
+app.put("/musicians/:id", async (request, response) => {
+    const newMusicians = request.body;
+    await Musician.update(
+        {name: newMusicians.name, instrument: newMusicians.instrument},
+        {where: {id: request.params.id}}
+    )
 
+    //const res = await Restaurant.findAll();
+    response.json(Musician);
+})
+//Deleting a musician based on ID
+app.delete("/musicians/:id", async (request, response) => {
+    const deletedMusician = await Musician.destroy({
+        where: {
+            id: request.params.id
+        }
+    })
+    response.json(Musician);
+})
 app.listen(port, () => {
     sequelize.sync();
-    console.log("Listening on port  http://localhost:3000/musicians/1")
+    console.log("Listening on port  http://localhost:3000/musicians")
 })
